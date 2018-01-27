57de237dfb179f63b14692ebc0c9a95cbc76332a
huangzhhui <huangzhwork@gmail.com>
2018-01-28 06:38:45 +0800

    remove useless config

--- test/config/properties/app.php
+++ test/config/properties/app.php
@@ -1,19 +1,9 @@
 <?php
 return [
-    "version"           => '1.0',
-    'autoInitBean'      => true,
-    'beanScan'          => [
-        'Swoft\\Redis\\Test\\Testing' => BASE_PATH."/Testing"
+    "version"      => '1.0',
+    'autoInitBean' => true,
+    'beanScan'     => [
+        'Swoft\\Redis\\Test\\Testing' => BASE_PATH . "/Testing"
     ],
-    'I18n'              => [
-        'sourceLanguage' => '@root/resources/messages/',
-    ],
-    'env'               => 'Base',
-    'user.stelin.steln' => 'fafafa',
-    'Service'           => [
-        'user' => [
-            'timeout' => 3000
-        ]
-    ],
-    'cache' => require dirname(__FILE__) . DS . "cache.php",
+    'cache'        => require __DIR__ . DS . "cache.php",
 ];
